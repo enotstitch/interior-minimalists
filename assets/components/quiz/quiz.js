d6b853a973b4ebc6e90dc120da683a51727ea1c7
enotstitch <kessler.anna96@gmail.com>
2023-11-06 00:36:05 +0300

    Стилизация и доработка функционала квиза

--- assets/components/quiz/quiz.js
+++ assets/components/quiz/quiz.js
@@ -2,32 +2,79 @@ import quizData from './quizData';
 
 const quizTemplate = (data = [], dataLength, options) => {
 	const { number, title } = data;
-	const { nextBtnText, prevBtnText } = options;
+	const { nextBtnText } = options;
 	const answers = data.answers.map((item) => {
-		return `
-
+		if (item.type === 'radio') {
+			return `
 			<label class="quiz-question__label">
-				<input type="${item.type}" data-valid="false" class="quiz-question__answer" name="${
-			data.answer_alias
-		}" ${item.type == 'text' ? 'placeholder="Введите ваш вариант"' : ''} value="${
-			item.type !== 'text' ? item.answer_title : ''
-		}">
-				<span>${item.answer_title}</span>
+					<input class="quiz-question__answer" type="radio" data-valid="false" name="${data.answer_alias}" value="${item.answer_title}">
+					<span>${item.answer_title}</span>
 			</label>
-
 		`;
+		} else if (item.type === 'text') {
+			return `
+			<label class="quiz-question__label">
+					<input class="quiz-question__answer" type="radio" data-valid="false" name="${data.answer_alias}">
+					<span>${item.answer_title}</span>
+					<input type="${item.type}" data-valid="false" class="quiz-question__answer" name="${data.answer_alias}">
+			</label>
+			`;
+		} else if (item.type === 'square') {
+			return `
+			<label class="quiz-question__label">
+					<input class="quiz-question__answer" type="text" data-valid="false" placeholder="М²" name="${data.answer_alias}">
+			</label>
+			`;
+		} else if (item.type === 'checkbox') {
+			return `
+			<label class="quiz-question__label">
+					<input class="quiz-question__answer" type="checkbox" data-valid="false" name="${data.answer_alias}" value="${item.answer_title}">
+					<span>${item.answer_title}</span>
+			</label>
+			`;
+		} else if (item.type === 'name') {
+			return `
+			<label class="quiz-question__label">
+					<input class="quiz-question__answer" type="text" data-valid="false" name="${data.answer_alias}" value="${item.answer_title}" placeholder="Ваше имя">
+					<span>${item.answer_title}</span>
+			</label>
+			`;
+		} else if (item.type === 'phone') {
+			return `
+			<label class="quiz-question__label">
+					<input class="quiz-question__answer" type="text" data-valid="false" name="${data.answer_alias}" value="${item.answer_title}">
+					<span>${item.answer_title}</span>
+			</label>
+			`;
+		} else if (item.type === 'policy') {
+			return `
+			<label class="quiz-question__label">
+					<input class="quiz-question__answer" type="checkbox" data-valid="false" name="${data.answer_alias}" value="${item.answer_title}">
+					<span>${item.answer_title}</span>
+					<p class="quiz-question__policy">
+						Согласен(на) на обработку персональных данных в соответствии с <a class="quiz-question__link">Политикой конфиденциальности</a>
+					</p>
+			</label>
+			`;
+		}
 	});
 
 	return `
 
 		<div class="quiz__content">
-			<div class="quiz__questions">${number} из ${dataLength}</div>
-			<div class="quiz-question">
-				<h3 class="quiz-question__title">${title}</h3>
+			<div class="quiz__header quiz-header">
+				<h3 class="quiz-header__title">${title}</h3>
+				<div class="quiz-header__num">0${number}<span class="quiz-header__num--gray">/0${dataLength}</span></div>
+			</div>
+		<div class="quiz-question">
 				<div class="quiz-question__answers">
 					${answers.join('')}
 				</div>
-				<button type="button" class="quiz-question__btn" data-next-btn>${nextBtnText}</button>
+				<div class="quiz-question__btns">
+				<button type="button" class="quiz-question__btn quiz-question__btn--margin btn-reset" data-next-btn>
+					<span>${nextBtnText}</span>
+				</button>
+				</div>
 			</div>
 		</div>
 
@@ -90,16 +137,22 @@ class Quiz {
 				this.$el.innerHTML = quizTemplate(quizData[this.counter], this.dataLength, this.options);
 
 				if (this.counter !== 0) {
-					this.$el.insertAdjacentHTML(
-						'beforeend',
-						`<button type="button" class="quiz-question__btn" data-prev-btn>${this.options.prevBtnText}</button>`,
-					);
+					const nextBtn = this.$el.querySelector('.quiz-question__btn');
+					nextBtn.outerHTML = `
+          <button type="button" class="quiz-question__btn quiz-question__btn--prev btn-reset" data-prev-btn><span>${this.options.prevBtnText}</span></button>
+          <button type="button" class="quiz-question__btn btn-reset" data-next-btn=""><span>${this.options.nextBtnText}</span></button>
+          `;
 				}
 
 				if (this.counter + 1 == this.dataLength) {
 					this.$el.insertAdjacentHTML(
 						'beforeend',
-						`<button type="button" class="quiz-question__btn" data-send>${this.options.sendBtnText}</button>`,
+						`<button type="button" class="quiz-question__send link btn-reset" data-send>
+						<span class="link__text">${this.options.sendBtnText}</span>
+							<svg class="link__svg">
+									<use href="img/icons/sprite.svg#arrow"></use>
+							</svg>
+						</button>`,
 					);
 					this.$el.querySelector('[data-next-btn]').remove();
 					this.$el.querySelector('[data-prev-btn]').remove();
@@ -115,10 +168,11 @@ class Quiz {
 		this.$el.innerHTML = quizTemplate(quizData[this.counter], this.dataLength, this.options);
 
 		if (this.counter !== 0) {
-			this.$el.insertAdjacentHTML(
-				'beforeend',
-				`<button type="button" class="quiz-question__btn" data-prev-btn>${this.options.prevBtnText}</button>`,
-			);
+			const nextBtn = this.$el.querySelector('.quiz-question__btn');
+			nextBtn.outerHTML = `
+          <button type="button" class="quiz-question__btn quiz-question__btn--prev btn-reset" data-prev-btn><span>${this.options.prevBtnText}</span></button>
+          <button type="button" class="quiz-question__btn btn-reset" data-next-btn=""><span>${this.options.nextBtnText}</span></button>
+          `;
 		}
 	}
 

7da771677d39c744e9cf2eb014f985d3850d38dc
ZayRexan <mogilevkostya@yandex.ru>
2023-11-12 04:03:37 +0700

    Добавление селектов и правки от заказчика

--- assets/js/functions/formValidate.js
+++
@@ -1,7 +0,0 @@
-const formValidate = () => {
-	let inputsPhone = document.querySelectorAll('input[type="tel"]');
-	let im = new Inputmask('+7 (999) 999-99-99');
-	im.mask(inputsPhone);
-};
-
-export default formValidate();
